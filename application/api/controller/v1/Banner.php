d2d2b4c9624ddaea47ac390acc8338f67fdd154b
LogiCadi <39653728+LogiCadi@users.noreply.github.com>
2019-07-04 19:41:16 +0800

    U

--- application/api/controller/v1/Banner.php
+++ application/api/controller/v1/Banner.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace app\api\controller\v1;
 
 use app\base\controller\Base;
 use app\api\model\Banner as BannerModel;
 use app\base\service\Common;
+use app\api\model\Article;
+use app\api\model\Fanclub;
 
 class Banner extends Base
 {
@@ -12,8 +15,13 @@ class Banner extends Base
     public function getList()
     {
         $banner_type = input('banner_type', 0);
-        $datas = BannerModel::all(['type' => $banner_type]);
+        $res['bannerList'] = BannerModel::all(['type' => $banner_type]);
 
-        Common::res(['data' => $datas]);
+        // 小banner
+        $artList = Article::whereTime('create_time', 'w')->field('name,"/pages/subPages/notice/list/list" as page')->select();
+        $fanclubList = Fanclub::where('status', 2)->field('concat(clubname,"已入驻") as name,"/pages/subPages/fanclub_list/fanclub_list" as page')->select();
+        $res['smallList'] = array_merge($artList, $fanclubList);
+
+        Common::res(['data' => $res]);
     }
 }
