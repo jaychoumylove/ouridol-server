7f740a7f639dfb0d6653446b872c37f068ee6da8
LogiCadi <39653728+LogiCadi@users.noreply.github.com>
2020-02-03 19:20:14 +0800

    f

--- application/api/controller/v1/StarRank.php
+++ application/api/controller/v1/StarRank.php
@@ -69,11 +69,9 @@ class StarRank extends Base
             $year = substr($value['date'], 0, 4);
             if ($rankField == 'week_hot') {
                 $week = substr($value['date'], -2);
-                // TODO:
-                $value['date'] = $year . '年' . ($week - 19) . '期';
+                $value['date'] = $year . '年' . $week . '周';
             } else if ($rankField == 'month_hot') {
                 $month = substr($value['date'], -2);
-
                 $value['date'] = $year . '年' . $month . '月';
             }
         }
