4888d5d5bf4dd7a646270ad19aaff54d17e00d1c
getUserKai <1664881221@qq.com>
2020-04-17 15:39:50 +0800

    edit

--- application/api/controller/v1/Signin.php
+++ application/api/controller/v1/Signin.php
@@ -22,11 +22,9 @@ class Signin extends Base
 {
     /**签到页面*/
     public function getSignin(){
-        $user_id=[];
         $this->getUser();
         $signinCfg=CfgDaySignin::select();
-        $starid = input('starid');
-        $user=DaySigninUser::where('star_id',$starid)->order('update_time asc')->select();
+        $star_id = input('starid');
         $gift_info=CfgSigninGift::all();
         foreach ($gift_info as $key=>&$value){
             $value['item_info']=json_decode(CfgItem::where('id','in',$value['item_id'])->find(),true);
@@ -39,37 +37,33 @@ class Signin extends Base
                 // 还没开始
                 $value['status'] = 1;
             } else if (time() >= $startTime && time() <= $endTime) {
+                $singninTime=CfgDaySignin::status();
+                $data['joinAll']=DaySigninUser::where(['signin_time'=>$singninTime['whole_time'],'star_id'=>$star_id])->count();
+                $data['isJoin']=DaySigninUser::where(['user_id'=>$this->uid,'signin_time'=>$singninTime['whole_time'],'star_id'=>$star_id])->count();
+                $data['timeLeft']=$singninTime['timeLeft'];
                 // 正签到时间段
-                foreach ($user as $k=>&$v){
-                        if(strtotime($v['signin_time'])==$startTime){
-                            $user_id[]=$v['user_id'];
-                    }
-                    $value['isJoin'] = DaySigninUser::where(['signin_time' => $value['signin_time'], 'user_id' => $this->uid])->count();
-
-                }
-
                 $value['status'] = 2;
             } else {
                 // 签到已结束
                 $value['status'] = 0;
             }
         }
-        $singninTime=CfgDaySignin::status();
-        $data['joinAll']=DaySigninUser::where(['signin_time'=>$singninTime['whole_time'],'star_id'=>$starid])->count();
-        $data['isJoin']=DaySigninUser::where(['user_id'=>$this->uid,'signin_time'=>$singninTime['whole_time'],'star_id'=>$starid])->count();
-        $data['timeLeft']=$singninTime['timeLeft'];
         $data['gift_info']=$gift_info;
-        $data['signinGroup']=user::where('id','in',$user_id)->field('id,nickname,avatarurl')->select();
+        $data['signinGroup']=[];
         $data['signinCfg']=$signinCfg;
         Common::res(['data' => $data]);
     }
     /** 签到*/
     public function joinSignin(){
         $this->getUser();
-        $starid=$this->req('starid','integer');
+        $star_id=$this->req('starid','integer');
         $type=$this->req('type','integer');
+        $nowTime=date('H:i',time());
+        $joinStatus=CfgDaySignin::where('start_time','<=',$nowTime)->where('end_time','>=',$nowTime)->find();
+        if(!$joinStatus){
+            Common::res(['code' => 2, 'msg' => '未到时间']);
+        }
         $singninTime=CfgDaySignin::status();
-        $singninTime['star']=StarModel::where('id',$starid)->value('name');
         if($type==1){
             $singninTime['coin']=$singninTime['coin']*10;
         }else{
@@ -79,10 +73,11 @@ class Signin extends Base
         if($isJoin){
             Common::res(['code' => 2, 'msg' => '已签到']);
         }
+
         Db::startTrans();
         try {
             // 新增签到用户
-            DaySigninUser::newSignin($this->uid,$starid,$singninTime['whole_time']);
+            DaySigninUser::newSignin($this->uid,$star_id,$singninTime['whole_time']);
             // 日志
             (new ServiceUser())->change($this->uid, [
                 'coin' => $singninTime['coin'],
@@ -94,40 +89,40 @@ class Signin extends Base
             Db::rollback();
             Common::res(['code' => 400, 'msg' => $e->getMessage()]);
         }
-        $this->sendGift($starid,$singninTime);
+        $this->sendGift($star_id,$singninTime);
         Common::res(['code'=>0,'data' => $singninTime]);
 
     }
-    public function sendGift($starid,$singninTime){
+    public function sendGift($star_id,$singninTime){
         //已签到人数
-        $siginedCount=DaySigninUser::where(['star_id'=>$starid,'signin_time'=>$singninTime['whole_time']])->count();
+        $siginedCount=DaySigninUser::where(['star_id'=>$star_id,'signin_time'=>$singninTime['whole_time']])->count();
 
         //签到结算 配置项
         $signGift = CfgSigninGift::where('count','<=',$siginedCount)->order('count desc')->find();
 
         //
-        $this->update_gift($starid,$signGift['sign_gift'],$signGift['item_id']);
+        $this->update_gift($star_id,$signGift['sign_gift'],$signGift['item_id']);
 
 
     }
 
     //更改礼物状态
-    public  function update_gift($starid,$sign_gift,$item_id){
+    public  function update_gift($star_id,$sign_gift,$item_id){
 
-        $isDone = StarModel::where(['id'=>$starid,'sign_gift'=>$sign_gift-1])->update([
+        $isDone = StarModel::where(['id'=>$star_id,'sign_gift'=>$sign_gift-1])->update([
             'sign_gift'=>$sign_gift
         ]);
 
-        if($isDone) $this->pushGift($starid,$item_id);
+        if($isDone) $this->pushGift($star_id,$item_id);
     }
 
-    public function pushGift($starid,$item_id){
+    public function pushGift($star_id,$item_id){
 
         $itemInfo=CfgItem::where('id',$item_id)->find();
-        $star=StarModel::where('id', $starid)->find();
+        $star=StarModel::where('id', $star_id)->find();
         Db::startTrans();
         try {
-            StarRankModel::where(['star_id' => $starid])->update([
+            StarRankModel::where(['star_id' => $star_id])->update([
                 'week_hot' => Db::raw('week_hot+' . $itemInfo['count']),
                 'month_hot' => Db::raw('month_hot+' . $itemInfo['count']),
             ]);
@@ -139,7 +134,7 @@ class Signin extends Base
         }
 
         // 圈内推送
-        Gateway::sendToGroup('star_' . $starid, json_encode([
+        Gateway::sendToGroup('star_' . $star_id, json_encode([
             'type' => 'sendItem',
             'data' => [
                 'itemicon' => $itemInfo['icon'],
--- application/api/model/DaySigninUser.php
+++ application/api/model/DaySigninUser.php
@@ -9,13 +9,17 @@ use app\base\service\Common;
 
 class DaySigninUser extends Base
 {
-    public static function newSignin($uid, $starid, $time)
+    public static function newSignin($uid, $star_id, $time)
     {
-        self::create([
-            'user_id'=>$uid,
-            'star_id' => $starid,
-            'signin_time' => $time,
-        ]);
+        $isRec=self::where(['user_id'=>$uid])->update(['signin_time'=>$time]);
+        if(!$isRec){
+            self::create([
+                'user_id'=>$uid,
+                'star_id' => $star_id,
+                'signin_time' => $time,
+            ]);
+        }
+
 
     }
 
