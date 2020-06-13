56bb57c64146fa831008bc6a99d0a0ab76743751
aladin <104460712@qq.com>
2020-06-13 14:08:55 +0800

    公众号服务整理

--- application/api/controller/v1/Notify.php
+++ application/api/controller/v1/Notify.php
@@ -1,26 +1,149 @@
 <?php
 namespace app\api\controller\v1;
 
+use app\api\model\User as UserModel;
+use app\api\model\GzhUser;
+use app\api\service\User as UserService;
 use app\base\service\WxMsg;
 use app\base\controller\Base;
 use app\base\service\WxAPI;
-use think\Log;
+use think\Db;
 
 class Notify extends Base
 {
 
+    private $wxMsg;
     public function receive()
     {
-        $wxMsg = new WxMsg(input('appid'));
-    
-        $wxMsg->checkSignature();
-        $msgFrom = $wxMsg->getMsg();
-        $msgTo = $wxMsg->msgHandler($msgFrom);        
-        if($msgTo['type']=='gzh'){
-            $wxMsg->autoSend($msgFrom,  $msgTo['MsgType'], ['Content' => $msgTo['content']]);
-        }
-    
+        $this->wxMsg = new WxMsg(input('appid'));
+
+        $this->wxMsg->checkSignature();
+        $msgFrom = $this->wxMsg->getMsg();
+        $this->msgHandler($msgFrom);
+
         die('success');
+    }
+
+    private function msgHandler($msgFrom)
+    {
+        if($this->wxMsg->appinfo['type']=='gzh') $this->msgGzh($msgFrom);
+        elseif($this->wxMsg->appinfo['type']=='miniapp') $this->msgMiniapp($msgFrom);
+    }
+
+    /**
+     * 处理小程序到的消息
+     * 并获取需要回复的消息
+     */
+    private function msgMiniapp($msg)
+    {
+        $Content = "你可能对以下内容感兴趣：\n";
+        $Content .= "关注公众号，公众号内回复“1”领取每日签到奖励\n";
+
+        //发送文本消息
+        $ret = (new WxAPI(input('appid')))->sendCustomerMsg(
+            $msg['FromUserName'],
+            'text',
+            [
+                'content' => $Content
+            ]
+        );
+
+        //发送不同的信息回复不同的内容
+        if (isset($msg['Content'])) {
+            if ($msg['Content'] == '1') {
+                // 充值
+                $media_id = $this->wxMsg->getMediaId(ROOT_PATH . 'public/uploads/cust-1.jpg');
+            } else if ($msg['Content'] == '2') {
+                // 打卡
+                $media_id = $this->wxMsg->getMediaId(ROOT_PATH . 'public/uploads/cust-2.jpg');
+
+            }else if ($msg['Content'] == '3') {
+                // 加群
+                $media_id = $this->wxMsg->getMediaId(ROOT_PATH . 'public/uploads/cust-3.jpg');
+            }
+
+            $ret = (new WxAPI(input('appid')))->sendCustomerMsg(
+                $msg['FromUserName'],
+                'image',
+                [
+                    'media_id' => $media_id
+                ]
+            );
+        }
+    }
+
+    //公众号处理
+    private function msgGzh($msg){
+
+        $Content = '';
+
+        if ($msg['MsgType'] == 'text' && isset($msg['Content']) && ($msg['Content'] == '2' || $msg['Content'] == '签到')) {
+            $Content .= $this->signDay($msg);
+
+        } elseif (isset($msg['Event']) && $msg['Event'] == 'CLICK' && $msg['EventKey'] == 'CLICK_kefu') { //按钮操作
+            $Content = " 【联系客服】\n请加客服（薇薇姐2）微信：ouridol2\n请一定注明反馈的问题或者建议，否则可能会被忽略哦！";
+
+        } elseif (isset($msg['Event']) && $msg['Event'] == 'CLICK' && $msg['EventKey'] == 'CLICK_lianxi') { //按钮操作
+            $Content = " 【商务合作】\n寻求合作及赞助可发送邮件：104460712@qq.com\n请一定注明公司、姓名、以及合作内容、品牌，否则可能会被忽略哦！";
+
+        } elseif (isset($msg['Event']) && ($msg['Event'] == 'subscribe' || $msg['Event'] == 'unsubscribe')) { //关注取关
+            $this->getUserId($msg);
+        }
+
+        $Content .= "你可能对以下内容感兴趣：\n";
+        $Content .= "回复“签到”领取每日签到奖励\n";
+        $Content .= "<a href='https://ouridol.anaculture.com/#/pages/recharge/recharge'>购买礼物</a>\n";
+        $Content .= "<a href='https://ouridol.anaculture.com/#/pages/prop/buy/buy'>购买道具</a>\n";
+        $Content .= "<a href='https://mp.weixin.qq.com/s/A2SmRYS5Xt1Qeh8tQVIxdg'>榜单福利</a>\n";
+        $Content .= "<a href='https://mp.weixin.qq.com/s/NRovcmTDj_Tziu8qe_DY9Q'>打榜攻略</a>\n";
+
+        $Content .= "\n";
+        $Content .= "<a data-miniprogram-appid='wx7dc912994c80d9ac' data-miniprogram-path='/pages/open/open' href='https://mp.weixin.qq.com/s/NRovcmTDj_Tziu8qe_DY9Q'>点击这里给爱豆打榜</a>";
+
+
+        $this->wxMsg->autoSend($msg, 'text', [
+            'Content' => $Content
+        ]);
+    }
+
+    /**
+     * 公众号每日签到
+     */
+    private function signDay($msg)
+    {
+        $user_id = $this->getUserId($msg);
+        if(!$user_id) return "没有关联到用户，请先到小程序打榜！\n<a data-miniprogram-appid=\"wx7dc912994c80d9ac\" data-miniprogram-path=\"/pages/index/index\">点击此链接去打榜吧~</a>\n----------------------------\n\n";
+
+        return "<a href='https://ouridol.anaculture.com/#/pages/signin/signin'>点这里每日签到</a>";
+
+//        $isSigned = UserExt::where('user_id', $user_id)->whereTime('gzh_signin_time', 'd')->value('id');
+//        if ($isSigned) return "你今天已经签到，明日再来！\n----------------------------\n\n";
+//
+//        // 增加货币
+//        $update = ['coin'=>3000,'stone'=>3];
+//        Db::startTrans();
+//        try {
+//            (new UserService)->change($user_id, $update,'公众号签到');
+//            UserExt::where('user_id', $user_id)->update(['gzh_signin_time' => time()]);
+//            Db::commit();
+//            return "签到成功，能量+{$update['coin']}，灵丹+{$update['stone']}\n----------------------------\n\n";
+//
+//        } catch (\Exception $e) {
+//            Db::rollBack();
+//            return 'rollBack:' . $e->getMessage();
+//        }
+    }
+
+    // 获取用户id
+    private function getUserId($msg)
+    {
+        $wxApi = new WxAPI(input('appid'));
+        $res = $wxApi->getUserInfocgi($wxApi->appinfo['access_token'],$msg['FromUserName']);
+        $user_id = isset($res['unionid']) ? UserModel::where(['unionid' => $res['unionid']])->value('id') : NULL;
+        $subscribe = (int) !($msg['MsgType'] == 'event' && $msg['Event'] == 'unsubscribe');//关注还是取关
+        GzhUser::gzhSubscribe(input('appid'), $user_id, $msg['FromUserName'], $subscribe);
+
+        return $user_id;
     }
     
     public function createMenu()
--- application/base/service/WxMsg.php
+++ application/base/service/WxMsg.php
@@ -2,17 +2,11 @@
 
 namespace app\base\service;
 
-use app\api\model\GzhTemplate;
-use app\api\model\GzhUser;
-use app\api\model\User;
-use app\api\model\UserExt;
 use app\api\model\WxImg;
-use think\Log;
-use app\api\model\Star;
 
 class WxMsg
 {
-    private $appinfo;
+    public $appinfo;
     public function __construct($w = null)
     {
         $this->appinfo = Common::getAppinfo($w);
@@ -105,150 +99,6 @@ class WxMsg
 
         $content = array_merge($content, $msgBody);
         die(Common::toXml($content));
-    }
-
-    public function msgHandler($msgFrom)
-    {    
-        if($this->appinfo['type']=='gzh') return $this->msgGzh($msgFrom);
-        elseif($this->appinfo['type']=='miniapp') return $this->msgMiniapp($msgFrom);
-    }
-    
-    /**
-     * 处理受到的消息
-     * 并获取需要回复的消息
-     */
-    public function msgMiniapp($msg)
-    {
-        if ($msg['MsgType'] == 'text') {
-            $media_id = $this->getMediaId(ROOT_PATH . 'public/uploads/cust-0.jpg');
-        
-            if (isset($msg['Content'])) {
-                if ($msg['Content'] == '1') {
-                    // 充值
-                    $media_id = $this->getMediaId(ROOT_PATH . 'public/uploads/cust-1.jpg');
-                } else if ($msg['Content'] == '2') {
-                    // 打卡
-                    $media_id = $this->getMediaId(ROOT_PATH . 'public/uploads/cust-2.jpg');
-        
-                }else if ($msg['Content'] == '3') {
-                    // 加群
-                    $media_id = $this->getMediaId(ROOT_PATH . 'public/uploads/cust-3.jpg');
-                }
-            }
-
-            $ret = (new WxAPI(input('appid')))->sendCustomerMsg(
-                $msg['FromUserName'],
-                'image',
-                [
-                    'media_id' => $media_id
-                ]
-            );
-        }
-    }
-    
-    
-    /**
-     * 处理受到的消息
-     * 并获取需要回复的消息
-     */
-    public function msgGzh($msg)
-    {
-        // 用户id
-        $user_id = $this->getUser($msg['FromUserName']);
-
-        $content = '欢迎！';
-        if ($msg['MsgType'] == 'text') {
-            // 文本消息
-            if (strpos($msg['Content'],'表白') === 0) {
-                
-                //查找爱豆
-                $star_name = str_replace("表白","",$msg['Content']);
-                $star_id = Star::where('name',$star_name)->value('id');
-                if(!$star_id) $content = "未找到你表白的爱豆，请先进入【我们的偶像】小程序打榜\n<a data-miniprogram-appid='wx7dc912994c80d9ac' data-miniprogram-path='/pages/open/open' href='https://mp.weixin.qq.com/s/NRovcmTDj_Tziu8qe_DY9Q'>点击这里给爱豆打榜</a>";
-                elseif (!$user_id) $content = "未找到用户，可能是因为您还未进入【我们的偶像】小程序打榜\n<a data-miniprogram-appid='wx7dc912994c80d9ac' data-miniprogram-path='/pages/open/open' href='https://mp.weixin.qq.com/s/NRovcmTDj_Tziu8qe_DY9Q'>点击这里给爱豆打榜</a>";
-                else {
-                    $content = UserExt::biaobai($user_id,$star_id,$star_name); 
-                    
-                    $media_id = $this->getMediaId(ROOT_PATH . 'public/uploads/biaobai520.png');
-                    $ret = (new WxAPI(input('appid')))->sendCustomerMsg($msg['FromUserName'], 'image', [
-                        'media_id' => $media_id
-                    ]);
-                }
-                
-            } else if ($msg['Content'] == '签到' || $msg['Content'] == 2) {
-                
-                // 每日签到
-                $content = "<a href='https://ouridol.anaculture.com/#/pages/signin/signin'>点这里每日签到</a>";
-                
-            }
-        } else if ($msg['MsgType'] == 'event') {
-            // 事件消息
-            if ($msg['EventKey'] == 'CLICK_kefu') {
-                
-                $content = " 【联系客服】\n您的用户ID为：" . ($user_id * 1234 ? $user_id * 1234 : '') . "\n请加客服（薇薇姐2）微信：ouridol2\n请一定注明反馈的问题或者建议，否则可能会被忽略哦！";
-            
-            } else if ($msg['EventKey'] == 'CLICK_lianxi') {
-                
-                $content = " 【商务合作】\n寻求合作及赞助可发送邮件：104460712@qq.com\n请一定注明公司、姓名、以及合作内容、品牌，否则可能会被忽略哦！";
-                
-            } else if ($msg['EventKey'] == 'biaobai520') { //
-
-                $media_id = $this->getMediaId(ROOT_PATH . 'public/uploads/biaobai520.png');
-                $ret = (new WxAPI(input('appid')))->sendCustomerMsg($msg['FromUserName'], 'image', [
-                    'media_id' => $media_id
-                ]);
-            } 
-            else if ($msg['Event'] == 'subscribe') {
-                
-                // 关注
-                $content = '谢谢你那么可爱还关注了我~';
-                
-            } else if ($msg['Event'] == 'subscribe') {
-                
-                // if ($user_id) {
-                //     // 匹配到用户
-
-                //     GzhTemplate::getTemplate(1, $msg['FromUserName'], 'wx7dc912994c80d9ac', 'https://idolzone.cyoor.com', [
-                //         '账号绑定成功', 
-                // User::where('id', $user_id)->value('nickname'),
-                //     ]);
-                // }
-                // $data = '{"touser":"oVR6g0keVmckJh257vfbtCMxYj0M","template_id":"4JUFNAzJFEbNJo5dOgEwI3bKSeOgS04q33J3110lz08","url":"https://idolzone.cyoor.com/","miniprogram":{"appid":"wx7dc912994c80d9ac"},"data":{"first":{"value":"你已绑定账号成功"},"keyword1":{"value":"巧克力"},"keyword2":{"value":"绑定成功,小程序订阅功能已开启"},"remark":{"value":">>>点击即可回到小程序订阅数据推送"}}}';
-                // $wxApi = new WxAPI($this->appinfo['appid']);
-                // $wxApi->sendMessageGzh($data);
-            }
-        }
-
-        $content .= "\n";
-        $content .= "\n你可能对以下内容感兴趣：";
-        $content .= "\n回复“签到”领取每日签到奖励";
-        $content .= "\n<a href='https://ouridol.anaculture.com/#/pages/recharge/recharge'>购买礼物</a>";
-        $content .= "\n<a href='https://ouridol.anaculture.com/#/pages/prop/buy/buy'>购买道具</a>";
-        $content .= "\n<a href='https://mp.weixin.qq.com/s/A2SmRYS5Xt1Qeh8tQVIxdg'>榜单福利</a>";
-        $content .= "\n<a href='https://mp.weixin.qq.com/s/NRovcmTDj_Tziu8qe_DY9Q'>打榜攻略</a>";
-
-        $content .= "\n";
-        $content .= "\n<a data-miniprogram-appid='wx7dc912994c80d9ac' data-miniprogram-path='/pages/open/open' href='https://mp.weixin.qq.com/s/NRovcmTDj_Tziu8qe_DY9Q'>点击这里给爱豆打榜</a>";
-        
-
-        // 关注/取关
-        $subscribe = (int) !($msg['MsgType'] == 'event' && $msg['Event'] == 'unsubscribe');
-        GzhUser::gzhSubscribe(input('appid'), $user_id, $msg['FromUserName'], $subscribe);
-
-        return ['type'=>'gzh','MsgType'=>'text','content'=>$content];
-    }
-
-    /**
-     * 公众号通过unionid获取唯一的用户id,需要关注公众号
-     * @param string $openid FromUserName
-     */
-    public function getUser($openid)
-    {
-        $wxApi = new WxAPI($this->appinfo['appid']);
-        $res = $wxApi->getUserInfocgi($openid);        
-        if (isset($res['unionid'])) {
-            return User::where(['unionid' => $res['unionid']])->value('id');
-        }
     }
 
     /**下载图片 */
