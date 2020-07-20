de387bfc5b78c7dd50d536e071cfeedbc4727527
zlf <2878253979@qq.com>
2020-07-20 14:09:49 +0800

    宝箱功能模块

--- application/api/controller/v1/TreasureBox.php
+++ application/api/controller/v1/TreasureBox.php
@@ -11,19 +11,22 @@ use app\base\service\Common;
 class TreasureBox extends Base
 {
 
+    protected $box_notice_id = 2;
     /**
      * 宝箱信息
      */
     public function info ()
     {
         $this->getUser();
-//        $res['flip_cards_count'] = (new UserExt())->where('user_id', $this->uid)->value('flip_cards_count');
+        $checkTimeInfo = UserTreasureBox::checkTime();
+        $res['nextTime']= $checkTimeInfo['nextTime'];
+        $res['nextTimeText']= $checkTimeInfo['nextTimeText'];
+        $res['box_notice_id']=$this->box_notice_id;//宝箱说明id
         $res['list'] = [[],[],[],[],[],[]];
 
         foreach ($res['list'] as $key => $value) {
 
-            $create_date_hour = UserTreasureBox::checkTime();
-            $is_get_info = UserTreasureBox::where('user_id', $this->uid)->where('index', $key)->where('create_date_hour',$create_date_hour)->find();
+            $is_get_info = UserTreasureBox::where('user_id', $this->uid)->where('index', $key)->where('create_date_hour',$checkTimeInfo['date'])->find();
             if ($is_get_info) {
 
                 $is_get_info['treasure_box']=CfgTreasureBox::get($is_get_info['treasure_box_id']);
@@ -41,14 +44,25 @@ class TreasureBox extends Base
      */
     public function open ()
     {
+        $this->getUser();
+        $user_id = input('user_id');
+        $index = input('index');
+        if (!$user_id) Common::res(['code' => 100]);
 
+        $res=UserTreasureBox::openBox($user_id,$this->uid,$index);
+        Common::res(['data'=>$res]);
     }
 
     /**
      * 宝箱记录
      */
-    public function log ()
+    public function log()
     {
+        $this->getUser();
+        $page = input('page', 1);
+        $size = input('size', 10);
+        $logList = UserTreasureBox::getList($this->uid, $page, $size);
 
+        Common::res(['data' => $logList]);
     }
 }
--- application/api/model/UserTreasureBox.php
+++ application/api/model/UserTreasureBox.php
@@ -8,19 +8,43 @@ use think\Model;
 
 class UserTreasureBox extends Base
 {
+    public static function getList($uid, $page, $size)
+    {
+        $logList = self::where(['user_id' => $uid])->order('id desc')->page($page, $size)->select();
+        $logList = json_decode(json_encode($logList, JSON_UNESCAPED_UNICODE), true);
 
-    public static function checkTime(){
-
-        $time1 = date('Y-m-d').'00';
-        $time2 = date('Y-m-d').'11';
-        $time3 = date('Y-m-d').'18';
-        if(date('Y-m-d H')>$time1 && date('Y-m-d H')<$time2){
-            $date = $time1;
-        }elseif (date('Y-m-d H')>$time2 && date('Y-m-d H')<$time3){
-            $date = $time2;
-        }else{
-            $date = $time3;
-        }
-        return $date;
+//        foreach ($logList as &$value) {
+//
+//        }
+        return $logList;
     }
+
+    public static function checkTime()
+    {
+
+        $time1 = date('Y-m-d') . ' 00:00:00';
+        $time2 = date('Y-m-d') . ' 11:00:00';
+        $time3 = date('Y-m-d') . ' 18:00:00';
+        if (date('Y-m-d H:i:s') > $time1 && date('Y-m-d H:i:s') < $time2) {
+            $res['date'] = $time1;
+            $res['nextTime'] = strtotime($time2) - time();
+            $res['nextTimeText'] = '11:00';
+        } elseif (date('Y-m-d H:i:s') > $time2 && date('Y-m-d H:i:s') < $time3) {
+            $res['date'] = $time2;
+            $res['nextTime'] = strtotime($time3) - time();
+            $res['nextTimeText'] = '18:00';
+        } else {
+            $res['date'] = $time3;
+            $res['nextTime'] = strtotime(date('Y-m-d', strtotime('+1 day'))) - time();
+            $res['nextTimeText'] = '00:00';
+        }
+        return $res;
+    }
+
+    public static function openBox($uid,$self,$index){
+
+    }
+
+
+
 }
